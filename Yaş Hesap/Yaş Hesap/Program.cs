7ac92c353a95d363c13caf53320e9e17e2fc983f
akifemre1907 <akifemre81@gmail.com>
2023-03-17 19:50:26 +0300

    Yas Hesaplama

---
+++ Yaş Hesap/Yaş Hesap/Program.cs
@@ -0,0 +1,7 @@
+﻿int bu_yil = Convert.ToInt32(DateTime.Now.Year);
+Console.Write("Doğum yılınızı girin: ");
+int dogum_yılı = Convert.ToInt16(Console.ReadLine());
+int yas;
+yas = bu_yil - dogum_yılı;
+Console.Write("Yaşınız= {0}", yas);
+Console.ReadKey();
